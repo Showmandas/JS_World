05b81145383e1c5b1cbff280c04df00a1fbb87ef
Showmandas <showmandas3216@gmail.com>
2026-03-09 10:40:56 +0600

    html,css and js file created for password generator project

---
+++ PasswordGenerator/script.js
@@ -0,0 +1,16 @@
+const lowerLetters="abcdefghijklmnopqrstuvwxyz"
+const upperLetters="ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+const numbers="0123456789"
+const symbols="!@#$%^&*(){}[]//,\"'./<>:?+=-_"
+
+const len=document.getElementById("length")
+const lower=document.getElementById("lowercase")
+const upper=document.getElementById("uppercase")
+const number=document.getElementById("numbers")
+const symbol=document.getElementById("symbols")
+const generate=document.getElementById("generate")
+const password=document.getElementById("password")
+
+generate=()=>{
+    
+}
